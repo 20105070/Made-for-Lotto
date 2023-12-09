02d83dba66a8a161fdc50f9b7089f822750301cf
20105070 <x20105070@student.ncirl.ie>
2023-12-09 13:58:11 +0000

    Commit madeforlotto and makeline 5.0

--- madeforlotto/config/routes.rb
+++ madeforlotto/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'makelines', to: 'pages#makelines'
-  get 'faqs', to: 'pages#faqs'
-  get 'loadlines', to: 'pages#loadlines'
-
-  #post '/makelines', :controller=>'pages', :action=>'generate'
-
-
+  resources :lines
+  get 'faqs', to: 'lines#faqs'
+  get 'loadlines', to: 'lines#loadlines'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#makelines"
+  root "lines#index"
 end
